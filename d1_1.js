1ed644db98212f886343a101a625d397357e31e7
joaodvargas <joao.vargasinacio@auth0.com>
2021-12-02 22:51:42 +0000

    advent day 1.1

---
+++ d1_1.js
@@ -0,0 +1,21 @@
+const readLines = require('./utils/readLines');
+try {
+  (async () => {
+    const lines = await readLines('d1_1.input');
+    if (lines) {
+      let count = 0;
+      lines.reduce((previous, current) => {
+        if (previous != null && +current > previous) {
+          count++;
+        }
+        return +current;
+      }, null);
+
+      console.log(`Descended ${count} times`);
+    } else {
+      console.log('No data found...');
+    }
+  })();
+} catch (err) {
+  console.log(err);
+}
