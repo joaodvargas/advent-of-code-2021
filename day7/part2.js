083912d49451290b332100602cf1e5f5c212c5c1
joaodvargas <joao.vargasinacio@auth0.com>
2021-12-07 09:50:27 +0000

    day 7.2

---
+++ day7/part2.js
@@ -0,0 +1,68 @@
+//const INPUT_FILE = 'example.in';
+const INPUT_FILE = 'input.in';
+
+const readLines = require('../utils/readLines');
+
+// entry point
+try {
+  (async () => {
+    const lines = await readLines(INPUT_FILE);
+    if (lines) {
+      const res = main(lines);
+      console.log(`The value you are looking for is ${res}`);
+    } else {
+      console.log('No data found... :(');
+    }
+  })();
+} catch (err) {
+  console.log(err);
+}
+
+// main code
+function main(lines) {
+  const initCrabPositions = lines[0].split(',').map((n) => +n);
+  //initCrabPositions.sort((a, b) => a - b);
+
+  const average = Math.floor(
+    initCrabPositions.reduce((acc, pos) => acc + pos, 0) /
+      initCrabPositions.length
+  );
+
+  // start from mid point, upwards and downwards, until we reach consensus
+  let optimalFuelSpent = calculateFuelToPoint(initCrabPositions, average);
+  let nextUp = average + 1,
+    nextDown = average - 1;
+  let increasingUp = false,
+    increasingDown = false;
+
+  while (true) {
+    if (increasingUp && increasingDown) {
+      break;
+    }
+    if (!increasingUp) {
+      const fuelSpent = calculateFuelToPoint(initCrabPositions, nextUp++);
+      if (fuelSpent < optimalFuelSpent) {
+        optimalFuelSpent = fuelSpent;
+      } else {
+        increasingUp = true;
+      }
+    }
+    if (!increasingDown) {
+      const fuelSpent = calculateFuelToPoint(initCrabPositions, nextDown--);
+      if (fuelSpent < optimalFuelSpent) {
+        optimalFuelSpent = fuelSpent;
+      } else {
+        increasingDown = true;
+      }
+    }
+  }
+
+  return optimalFuelSpent;
+}
+
+// fuel spent increases by +1 with every move, so its sum of all numbers from 0 to distance
+const calculateFuelToPoint = (crabs, position) =>
+  crabs.reduce((acc, crab) => {
+    const distance = Math.abs(crab - position);
+    return acc + (distance * (distance + 1)) / 2;
+  }, 0);
