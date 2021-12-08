220599c1b69822aa8b07ecd0e87b89d364d3c6a7
joaodvargas <joao.vargasinacio@auth0.com>
2021-12-08 19:21:12 +0000

    commit template

---
+++ template/part1.js
@@ -0,0 +1,28 @@
+const INPUT_FILE = 'example.in';
+//const INPUT_FILE = 'input.in';
+
+const readLines = require('../utils/readLines');
+
+// entry point
+try {
+  (async () => {
+    const lines = await readLines(INPUT_FILE);
+    if (lines) {
+      const res = main(lines);
+      console.log(`The value you are looking for is ${res}`);
+    } else {
+      console.log('No data found... :(');
+    }
+  })();
+} catch (err) {
+  console.log(err);
+}
+
+// main code
+function main(lines) {
+  lines.forEach((line) => {
+    // do things...
+  });
+
+  return 42;
+}
