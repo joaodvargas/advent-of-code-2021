ade6681366b0f9c1dc60260dfc60547d319403f1
Joao Inacio <joaodvargas@gmail.com>
2021-12-13 22:29:52 +0000

    template & debug tweaks

--- template/part1.js
+++ template/part1.js
@@ -3,26 +3,29 @@ const INPUT_FILE = 'example.in';
 
 const readLines = require('../utils/readLines');
 
-// entry point
-try {
-  (async () => {
-    const lines = await readLines(INPUT_FILE);
-    if (lines) {
-      const res = main(lines);
-      console.log(`The value you are looking for is ${res}`);
-    } else {
-      console.log('No data found... :(');
-    }
-  })();
-} catch (err) {
-  console.log(err);
-}
-
 // main code
-function main(lines) {
-  lines.forEach((line) => {
-    // do things...
-  });
-
-  return 42;
+async function main(fileLocation) {
+  const input = await parseInput(fileLocation);
+  const solution = solve(input);
+  console.log(`The value you are looking for is ${solution}`);
 }
+
+// operate on input to solve problem
+const solve = (input) => {
+  // ...
+  return 42;
+};
+
+// read file input into data structure(s)
+const parseInput = async (fileLocation) => {
+  const lines = await readLines(fileLocation);
+  if (!lines) {
+    throw Exception('No data found... :(');
+  }
+
+  // parse lines
+  // ...
+};
+
+// run
+main(process.argv[2] || INPUT_FILE);
