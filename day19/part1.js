d49c33186158e13628c1e67f75771c22edf2dcd8
Joao Inacio <joaodvargas@gmail.com>
2021-12-21 00:07:29 +0000

    day 19 wip

---
+++ day19/part1.js
@@ -0,0 +1,407 @@
+//const INPUT_FILE = 'example-short.in';
+const INPUT_FILE = 'example.in';
+//const INPUT_FILE = 'input.in';
+
+const readLines = require('../utils/readLines');
+
+// main code
+function main(filePath) {
+  parseInput(filePath).then((input) => {
+    const solution = solve(input);
+    console.log(`The value you are looking for is ${solution}`);
+  });
+}
+
+// operate on input to solve problem
+const solve = (scanners) => {
+  // calculate local probe distance
+  scanners.forEach(calculateProbeDistance);
+
+  // look for overlaps
+  for (let s1Index = 0; s1Index < scanners.length; s1Index++) {
+    for (let s2Index = s1Index + 1; s2Index < scanners.length; s2Index++) {
+      const scanner1 = scanners[s1Index],
+        scanner2 = scanners[s2Index];
+
+      // compare probe by probe
+      const matchingProbes = findProbesWithSameDistanceToOtherProbes(
+        scanner1,
+        scanner2
+      );
+
+      if (matchingProbes.length >= 12) {
+        console.log(
+          `Found ${matchingProbes.length} probes shared between scanners ${s1Index} and ${s2Index}`
+        );
+        let resolvedLocation = false;
+        // use to resolve location relative to origin - scanner 1 location
+        for (
+          let matchIndex = 0;
+          matchIndex < matchingProbes.length;
+          matchIndex++
+        ) {
+          if (resolvedLocation) {
+            break;
+          }
+          const [p1Index, p2Index] = matchingProbes[matchIndex];
+          const probe1 = scanner1.probes[p1Index];
+          const probe2 = scanner2.probes[p2Index];
+          // look for x,y,z adjustments
+          for (let p1Distance of probe1.distances.filter((d) => d != null)) {
+            // matching p2 distance
+            const p2Distance = probe2.distances
+              .filter((d) => d != null)
+              .find((d) => d.total === p1Distance.total);
+            if (p2Distance == null) {
+              continue;
+            }
+
+            const calibration = calibrateAxisBasedOnDistance(
+              p1Distance,
+              p2Distance
+            );
+            if (calibration != null) {
+              // we can calculate distance from probe to source
+              const p2CalibratedDistance = applyCalibrationToCoordinates(
+                probe2,
+                calibration
+              );
+
+              const relativePositionToScanner1 = getDistanceBetweenCoordinates(
+                probe1,
+                p2CalibratedDistance
+              );
+
+              // found position of scanner2 relative to scanner1!
+              scanner1.relativePosition[scanner2.id] =
+                relativePositionToScanner1;
+              scanner1.relativeCalibration[scanner2.id] = calibration;
+
+              // execute same logic in regards to s2 -> s1
+              const reverseCalibration = calibrateAxisBasedOnDistance(
+                p2Distance,
+                p1Distance
+              );
+              if (reverseCalibration == null) {
+                throw Error('Unexpected null calibration found.');
+              }
+              const p1CalibratedDistance = applyCalibrationToCoordinates(
+                probe1,
+                reverseCalibration
+              );
+              const relativePositionToScanner2 = getDistanceBetweenCoordinates(
+                probe2,
+                p1CalibratedDistance
+              );
+              scanner2.relativePosition[scanner1.id] =
+                relativePositionToScanner2;
+              scanner2.relativeCalibration[scanner1.id] = reverseCalibration;
+
+              resolvedLocation = true;
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  const s0 = scanners[0],
+    s1 = scanners[1],
+    s2 = scanners[2],
+    s4 = scanners[4];
+
+  // test for 4 -> 1 -> 0
+  const calibratedPosition4to0axis = applyCalibrationToCoordinates(
+    s1.relativePosition[4],
+    s0.relativeCalibration[1]
+  );
+  const position4to0 = addUpCoordinates(
+    s0.relativePosition[1],
+    calibratedPosition4to0axis
+  );
+  // test for 3 -> 1 -> 0
+  const calibratedPosition3to0axis = applyCalibrationToCoordinates(
+    s1.relativePosition[3],
+    s0.relativeCalibration[1]
+  );
+  const position3to0 = addUpCoordinates(
+    s0.relativePosition[1],
+    calibratedPosition3to0axis
+  );
+
+  // test for 2 -> 4 -> 1 -> 0
+  const calibratedPosition2to1axis = applyCalibrationToCoordinates(
+    s4.relativePosition[2],
+    s1.relativeCalibration[4]
+  );
+  const calibration2to1to0axis = applyCalibrationToCoordinates(
+    calibratedPosition2to1axis,
+    s0.relativeCalibration[1]
+  );
+  const position2to0 = addUpCoordinates(position4to0, calibration2to1to0axis);
+
+  const calibration2to1to0axisv2 = applyCalibrationToCoordinates(
+    s4.relativePosition[2],
+    [s1.relativeCalibration[4], s0.relativeCalibration[1]]
+  );
+  const position2to0v2 = addUpCoordinates(position4to0, calibration2to1to0axis);
+
+  return 42;
+};
+
+// const getCoordinates = (obj) => ({
+//   x: obj.x,
+//   y: obj.y,
+//   z: obj.z,
+// });
+
+// const calibrationToStr = (calibration) => {
+//   const mapIndexToLetter = {
+//     [0]: 'x',
+//     [1]: 'y',
+//     [2]: 'z',
+//   };
+
+//   let str = '[';
+
+//   // x
+//   str += calibration.x[1] < 0 ? '-' : '';
+//   str += mapIndexToLetter[calibration.x[0]];
+//   str += ', ';
+//   // y
+//   str += calibration.y[1] < 0 ? '-' : '';
+//   str += mapIndexToLetter[calibration.y[0]];
+//   str += ', ';
+//   // z
+//   str += calibration.z[1] < 0 ? '-' : '';
+//   str += mapIndexToLetter[calibration.z[0]];
+//   str += ']';
+//   return str;
+// };
+
+const calibrateAxisBasedOnDistance = (distance1, distance2) => {
+  // if we have same distances, go to next
+  const distanceSet = new Set([
+    Math.abs(distance1.x),
+    Math.abs(distance2.x),
+    Math.abs(distance1.y),
+    Math.abs(distance2.y),
+    Math.abs(distance1.z),
+    Math.abs(distance2.z),
+  ]);
+  if (distanceSet.size < 3) {
+    return null;
+  }
+
+  for (let idx = 0; idx < possibleCalibrations.length; idx++) {
+    const calibration = possibleCalibrations[idx];
+    const calibratedDistance = applyCalibrationToCoordinates(
+      distance2,
+      calibration
+    );
+    if (
+      distance1.x === calibratedDistance.x &&
+      distance1.y === calibratedDistance.y &&
+      distance1.z === calibratedDistance.z
+    ) {
+      return calibration;
+    }
+  }
+  throw Error('Unable to find meaningful calibration');
+};
+
+// const applyCalibrationToCalibration = (originalCalibration, modifier) => {
+//   const newCalibration = [0, 0, 0];
+//   newCalibration[modifier.x[0]] = [
+//     originalCalibration.x[0],
+//     originalCalibration.x[1] * modifier.x[1],
+//   ];
+//   newCalibration[modifier.y[0]] = [
+//     originalCalibration.y[0],
+//     originalCalibration.y[1] * modifier.y[1],
+//   ];
+//   newCalibration[modifier.z[0]] = [
+//     originalCalibration.z[0],
+//     originalCalibration.z[1] * modifier.z[1],
+//   ];
+
+//   return {
+//     x: newCalibration[0],
+//     y: newCalibration[1],
+//     z: newCalibration[2],
+//   };
+// };
+
+const applyCalibrationToCoordinates = (coordinates, calibrations) => {
+  if (!Array.isArray(calibrations)) {
+    calibrations = [calibrations];
+  }
+
+  const newCoordinates = [coordinates.x, coordinates.y, coordinates.z];
+  for (let cIndex = 0; cIndex < calibrations.length; cIndex++) {
+    const calibration = calibrations[cIndex];
+    const workingCoordinates = [...newCoordinates];
+    newCoordinates[calibration.x[0]] = workingCoordinates[0] * calibration.x[1];
+    newCoordinates[calibration.y[0]] = workingCoordinates[1] * calibration.y[1];
+    newCoordinates[calibration.z[0]] = workingCoordinates[2] * calibration.z[1];
+  }
+
+  return {
+    x: newCoordinates[0],
+    y: newCoordinates[1],
+    z: newCoordinates[2],
+  };
+};
+
+const getDistanceBetweenCoordinates = (c1, c2) => ({
+  x: c1.x - c2.x,
+  y: c1.y - c2.y,
+  z: c1.z - c2.z,
+});
+
+const addUpCoordinates = (c1, c2) => ({
+  x: c1.x + c2.x,
+  y: c1.y + c2.y,
+  z: c1.z + c2.z,
+});
+
+// Possible axis orientations, with [<place in x,y,z>, <forward or backward>]
+const possibleCalibrations = [
+  { x: [0, 1], y: [1, 1], z: [2, 1] }, // x, y, z
+  { x: [0, 1], y: [2, 1], z: [1, -1] }, // x, z, -y
+  { x: [0, 1], y: [1, -1], z: [2, -1] }, // x, -y, -z
+  { x: [0, 1], y: [2, -1], z: [1, 1] }, // x, -z, y
+
+  { x: [0, -1], y: [1, 1], z: [2, -1] }, // -x, y, -z
+  { x: [0, -1], y: [2, -1], z: [1, -1] }, // -x, -z, -y
+  { x: [0, -1], y: [1, -1], z: [2, 1] }, // -x, -y, z
+  { x: [0, -1], y: [2, 1], z: [1, 1] }, // -x, z, y
+
+  { x: [1, 1], y: [0, -1], z: [2, 1] }, // y, -x, z
+  { x: [2, -1], y: [0, -1], z: [1, 1] }, // -z, -x, y
+  { x: [1, -1], y: [0, -1], z: [2, -1] }, // -y, -x, -z
+  { x: [2, 1], y: [0, -1], z: [1, -1] }, // z, -x, -y
+
+  { x: [1, -1], y: [0, 1], z: [2, 1] }, // -y, x, z
+  { x: [2, -1], y: [0, 1], z: [1, -1] }, // -z, x, -y
+  { x: [1, 1], y: [0, 1], z: [2, -1] }, // y, x, -z
+  { x: [2, 1], y: [0, 1], z: [1, 1] }, // z, x, y
+
+  { x: [2, -1], y: [1, 1], z: [0, 1] }, // -z, y, x
+  { x: [1, 1], y: [2, 1], z: [0, 1] }, // y, z, x
+  { x: [2, 1], y: [1, -1], z: [0, 1] }, // z, -y, x
+  { x: [1, -1], y: [2, -1], z: [0, 1] }, // -y, -z, x
+
+  { x: [2, 1], y: [1, 1], z: [0, -1] }, // z, y, -x
+  { x: [1, 1], y: [2, -1], z: [0, -1] }, // y, -z, -x
+  { x: [2, -1], y: [1, -1], z: [0, -1] }, // -z, -y, -x
+  { x: [1, -1], y: [2, 1], z: [0, -1] }, // -y, z, -x
+];
+
+const findProbesWithSameDistanceToOtherProbes = (scanner1, scanner2) => {
+  // compare probe by probe
+  const possibleMatch = [];
+  for (
+    let s1probeIndex = 0;
+    s1probeIndex < scanner1.probes.length;
+    s1probeIndex++
+  ) {
+    // can't find 12 overlapping probes, abort early
+    if (possibleMatch.length + (scanner1.probes.length - s1probeIndex) < 12) {
+      break;
+    }
+
+    for (
+      let s2probeIndex = 0;
+      s2probeIndex < scanner2.probes.length;
+      s2probeIndex++
+    ) {
+      const scanner1Probe = scanner1.probes[s1probeIndex];
+      const scanner2Probe = scanner2.probes[s2probeIndex];
+
+      const possibleOverlaps = scanner1Probe.totalDistances.filter((d) =>
+        scanner2Probe.totalDistances.includes(d)
+      );
+      if (possibleOverlaps.length >= 11) {
+        // found matching probe
+        scanner2Probe.isDuplicate = true;
+        possibleMatch.push([s1probeIndex, s2probeIndex]);
+        break;
+      }
+    }
+  }
+
+  return possibleMatch;
+};
+
+const calculateProbeDistance = ({ id, probes }) => {
+  for (let p1Index = 0; p1Index < probes.length; p1Index++) {
+    for (let p2Index = p1Index + 1; p2Index < probes.length; p2Index++) {
+      // dist between every probe
+      const p1 = probes[p1Index],
+        p2 = probes[p2Index];
+      const x = p2.x - p1.x,
+        y = p2.y - p1.y,
+        z = p2.z - p1.z;
+
+      p1.distances = p1.distances || [];
+      p1.distances[p2.id] = {
+        toProbe: p2.id,
+        x,
+        y,
+        z,
+        total: Math.abs(x) + Math.abs(y) + Math.abs(z),
+      };
+      p2.distances = p2.distances || [];
+      p2.distances[p1.id] = {
+        toProbe: p1.id,
+        x: -x,
+        y: -y,
+        z: -z,
+        total: Math.abs(x) + Math.abs(y) + Math.abs(z),
+      };
+    }
+    probes[p1Index].totalDistances = probes[p1Index].distances
+      .map((d) => d.total)
+      .sort((a, b) => a - b);
+  }
+};
+
+// read file input into data structure(s)
+const parseInput = async (filePath) => {
+  const lines = await readLines(filePath);
+  if (!lines || !lines.length) {
+    throw Error('No data found... :(');
+  }
+
+  const scanners = [];
+
+  let scannerIndex = 0;
+  let probeIndex = 0;
+  for (line of lines) {
+    if (line.indexOf('---') > -1) {
+      // new probe
+      scanners.push({
+        id: scannerIndex++,
+        probes: [],
+        relativePosition: [],
+        relativeCalibration: [],
+      });
+      probeIndex = 0;
+    } else {
+      const [x, y, z] = line.split(',').map((n) => +n);
+      scanners[scannerIndex - 1].probes.push({
+        id: probeIndex++,
+        x,
+        y,
+        z,
+      });
+    }
+  }
+
+  return scanners;
+};
+
+// run
+main(process.argv[2] || INPUT_FILE);
