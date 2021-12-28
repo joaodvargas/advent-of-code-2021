26d63a8ba05009783fd7d5ae055f254fcd485921
Joao Inacio <joaodvargas@gmail.com>
2021-12-28 01:51:35 +0000

    day 24.1

--- day24/part1.js
+++ day24/part1.js
@@ -14,29 +14,6 @@ function main(filePath) {
     console.log(`Took ${Math.ceil(endTime - startTime)}ms to find it`);
   });
 }
-// Operations & Variables --
-const getEmptyVariables = () => ({
-  w: 0,
-  x: 0,
-  y: 0,
-  z: 0,
-});
-
-const getOperator = (variables) => {
-  const getVariableOrNumber = (b) => (isNaN(b) ? variables[b] : +b);
-  return {
-    inp: (a, b) => {
-      variables[a] = b;
-    },
-    add: (a, b) => (variables[a] = variables[a] + getVariableOrNumber(b)),
-    mul: (a, b) => (variables[a] = variables[a] * getVariableOrNumber(b)),
-    div: (a, b) =>
-      (variables[a] = Math.floor(variables[a] / getVariableOrNumber(b))),
-    mod: (a, b) => (variables[a] = variables[a] % getVariableOrNumber(b)),
-    eql: (a, b) =>
-      (variables[a] = variables[a] === getVariableOrNumber(b) ? 1 : 0),
-  };
-};
 
 // ------
 // Run --
@@ -46,77 +23,48 @@ const INPUT_NUMBER_SIZE = 14;
 const INPUT_REPETITION = 18;
 
 const solve = (instructions) => {
-  let inputNumber = Array(INPUT_NUMBER_SIZE).fill(9);
-  let cachedResults = Array(INPUT_NUMBER_SIZE).fill(null);
-  let validCacheIndex = -1;
+  // had to look up help, although I had reached the conclusion it had to be based on input.
+  // It's a base 26 number, with repeats of instructions pushing and popping numbers based on the "div z 1 | 26" line
+  // We can boil it down to simple expressions to determine the conditionals of how the push must match the pop, to eventually get back to 0
+  // Formula is "prev_w + (arg2 in add x 5th line)" == "now_w - (arg2 in add y 15th line)"
+  // NOTE: input is unique to everyone!
+  // https://www.reddit.com/r/adventofcode/comments/rnejv5/comment/hps5hgw/?utm_source=share&utm_medium=web2x&context=3
 
-  const advanceNumber = () => {
-    // reset cache to best possible value
-    validCacheIndex = INPUT_NUMBER_SIZE - 2;
-
-    for (let idx = inputNumber.length - 1; idx >= 0; idx--) {
-      if (inputNumber[idx] === 1) {
-        inputNumber[idx] = 9;
-        // clear cache as we move towards a new digit #, and move backwards on the valid cache index
-        console.log(cachedResults);
-        cachedResults[validCacheIndex--] = null;
-      } else {
-        inputNumber[idx]--;
-        break;
+  const resolveDigits = (prevAddY, currAddX) => {
+    for (let previousDigit = 9; previousDigit > 0; previousDigit--) {
+      for (let currentDigit = 9; currentDigit > 0; currentDigit--) {
+        const pushed = previousDigit + prevAddY,
+          popped = currentDigit - currAddX;
+        if (pushed === popped) {
+          return [previousDigit, currentDigit];
+        }
       }
     }
+    throw Error('Unable to resolve digits!');
   };
 
-  while (true) {
-    // check for possible cache
-    const variables =
-      cachedResults[validCacheIndex] != null
-        ? { ...cachedResults[validCacheIndex] }
-        : getEmptyVariables();
-    const startingInstructionIndex = Math.max(
-      0,
-      (validCacheIndex + 1) * INPUT_REPETITION
-    );
-    const iterationOperations = getOperator(variables);
-    let firstInputRead = true;
+  const serialNumber = Array(INPUT_NUMBER_SIZE).fill(null);
+  const stack = [];
+  let digitPosition = 0;
+  for (let idx = 0; idx < instructions.length; idx = idx + INPUT_REPETITION) {
+    const inputInstructions = instructions.slice(idx, idx + INPUT_REPETITION);
+    const isPush = +inputInstructions[4].arg2 === 1;
 
-    for (let idx = startingInstructionIndex; idx < instructions.length; idx++) {
-      const { op, arg1, arg2 } = instructions[idx];
-
-      if (idx % INPUT_REPETITION === 0) {
-        if (op !== 'inp') {
-          throw Error(`Unexpected op at the input index: ${op}`);
-        }
-        // cache the value calculated up to this stage, unless this is the very first input we are reading
-        if (!firstInputRead) {
-          cachedResults[idx / INPUT_REPETITION - 1] = { ...variables };
-        } else {
-          firstInputRead = false;
-        }
-        // read input
-        iterationOperations.inp(arg1, inputNumber[idx / INPUT_REPETITION]);
-      } else {
-        iterationOperations[op](arg1, arg2);
-      }
-
-      // console.log(`${op} ${arg1} ${arg2}`);
-      // console.log(
-      //   `w:${variables.w}, x:${variables.x}, y:${variables.y}, z:${variables.z}`
-      // );
-    }
-
-    // console.log(
-    //   `${inputNumber.join('')},` +
-    //     `w:${variables.w}, x:${variables.x}, y:${variables.y}, z:${variables.z}`
-    // );
-
-    // Valid and highest
-    if (variables.z === 0) {
-      return inputNumber.join('');
+    if (isPush) {
+      stack.push([digitPosition, +inputInstructions[15].arg2]);
     } else {
-      advanceNumber();
+      const [prevPosition, prevValue] = stack.pop();
+      const currentValue = +inputInstructions[5].arg2;
+
+      const [prevDigit, currentDigit] = resolveDigits(prevValue, currentValue);
+      serialNumber[prevPosition] = prevDigit;
+      serialNumber[digitPosition] = currentDigit;
     }
+
+    digitPosition++;
   }
+
+  return serialNumber.join('');
 };
 
 // read file input into data structure(s)
